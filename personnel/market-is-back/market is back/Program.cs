7bb0bdabb4531d485ff541345fccc982a1c0b168
Thomas <163824101+ThomasDelETML@users.noreply.github.com>
2025-09-10 12:09:54 +0200

    Feat (MiB) : Partie 1, transform pour afficher un résultat modifier de la list

---
+++ personnel/market-is-back/market is back/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace market_is_back
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>
+            {
+                new Product { Location = 1, Producer = "Bornand", ProductName = "Pommes", Quantity = 20,Unit = "kg", PricePerUnit = 5.50 },
+                new Product { Location = 1, Producer = "Bornand", ProductName = "Poires", Quantity = 16,Unit = "kg", PricePerUnit = 5.50 },
+                new Product { Location = 1, Producer = "Bornand", ProductName = "Pastèques", Quantity = 14,Unit = "pièce", PricePerUnit = 5.50 },
+                new Product { Location = 1, Producer = "Bornand", ProductName = "Melons", Quantity = 5,Unit = "kg", PricePerUnit = 5.50 },
+                new Product { Location = 2, Producer = "Dumont", ProductName = "Noix", Quantity = 20,Unit = "sac", PricePerUnit = 5.50 },
+                new Product { Location = 2, Producer = "Dumont", ProductName = "Raisin", Quantity = 6,Unit = "kg", PricePerUnit = 5.50 },
+                new Product { Location = 2, Producer = "Dumont", ProductName = "Pruneaux", Quantity = 13,Unit = "kg", PricePerUnit = 5.50 },
+                new Product { Location = 2, Producer = "Dumont", ProductName = "Myrtilles", Quantity = 12,Unit = "kg", PricePerUnit = 5.50 },
+
+                //...
+            };
+
+            var dictionary = new Dictionary<string, string>()
+            {
+                { "Pommes","Apples"},
+                { "Poires","Pears"},
+                { "Pastèques","Watermelons"},
+                { "Melons","Melons"},
+                { "Noix","Nuts"},
+                { "Raisin","Grapes"},
+                { "Pruneaux","Plums"},
+                { "Myrtilles","Blueberries"},
+                { "Groseilles","Berries"},
+                { "Tomates","Tomatoes"},
+                { "Courges","Pumpkins"},
+                { "Pêches","Peaches"},
+                { "Haricots","Beans"}
+            };
+
+            //Prend 3premier V     V Les colles avec             Le dernier V
+            var transformed = products.Select(p => new Product 
+            {
+                Producer = new string(p.Producer
+                .Take(3)
+                .Concat("...")
+                .Concat(new[] { p.Producer.Last() })
+                .ToArray()),
+
+                ProductName = dictionary[p.ProductName],
+                CA = p.PricePerUnit * p.Quantity,
+            });
+
+            Console.WriteLine("Seller\tProduct\tCA");
+            foreach (var item in transformed)
+            {
+                Console.WriteLine($"{item.Producer}\t{item.ProductName}\t{item.CA}");
+            }
+        }
+    }
+}
