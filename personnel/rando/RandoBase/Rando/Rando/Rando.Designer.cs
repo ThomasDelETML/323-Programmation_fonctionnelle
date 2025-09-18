55fb8b22f5714bc5f2237fd8867d2d803fad6d9a
Thomas Moreira <po51oro@eduvaud.ch>
2025-09-18 09:58:31 +0200

    feat (Rando) : Transformation
    Oublier de faire les commits hier

--- personnel/rando/RandoBase/Rando/Rando/Rando.Designer.cs
+++ personnel/rando/RandoBase/Rando/Rando/Rando.Designer.cs
@@ -30,12 +30,12 @@
         {
             SuspendLayout();
             // 
-            // Form1
+            // Rando
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(700, 1000);
-            Name = "Form1";
+            Name = "Rando";
             Text = "Rando";
             Paint += Rando_Form_Paint;
             ResumeLayout(false);
