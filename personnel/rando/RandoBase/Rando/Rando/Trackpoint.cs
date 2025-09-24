ddbc3fa6f475193566174a016d982f47a506465b
Thomas <163824101+ThomasDelETML@users.noreply.github.com>
2025-09-24 09:24:42 +0200

    feat (Rando) : Lecture

---
+++ personnel/rando/RandoBase/Rando/Rando/Trackpoint.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rando
+{
+    internal class Trackpoint
+    {
+            private double _latitude;
+            private double _longitude;
+            private double _elevation;
+    }
+}
